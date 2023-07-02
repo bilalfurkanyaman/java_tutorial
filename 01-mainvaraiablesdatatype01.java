d29ccf0e65567fb79dce8a9fca3cbbf8db96ff78
bilalfurkanyaman <138367431+bilalfurkanyaman@users.noreply.github.com>
2023-07-02 13:13:11 +0300

    01

---
+++ 01-mainvaraiablesdatatype01.java
@@ -0,0 +1,20 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Main
+{
+    public static void main(String[] args) {
+        byte ageStudent = 13;
+        char carName = 'volkswagen';
+        boolean okulaGidiyorMusun = true;
+        short villagePopulation = 31520;
+        int countryPopulation = 85512315;
+        long worldPopulationn = 70000000000L;
+        long weightOfSun = 1234567; //ton olarak yazdım kilogram olarak çevirilirse long olcaktır ama ton olarak inttir 
+        
+    }
+}

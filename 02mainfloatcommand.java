31075f3832de31fedfc851beb4984bc5e41c5627
bilalfurkanyaman <138367431+bilalfurkanyaman@users.noreply.github.com>
2023-07-02 13:30:12 +0300

    02

---
+++ 02mainfloatcommand.java
@@ -0,0 +1,12 @@
+
+
+public class Main
+
+
+{
+    public static void main(String[] args) {
+    float shoesPrice = 25.42F;  
+    float shirtPrice = 36.52F;    
+    System.out.println(shirtPrice + shoesPrice);    
+    }
+}

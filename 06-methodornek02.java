56ee756c88878126b1af67680a1985918e681c61
bilalfurkanyaman <138367431+bilalfurkanyaman@users.noreply.github.com>
2023-07-02 15:09:14 +0300

    06

---
+++ 06-methodornek02.java
@@ -0,0 +1,13 @@
+
+
+public class Main{
+    public static void main(String[] args) {
+       double kub = getCube(5);
+       System.out.println(kub);
+       
+    
+    }
+    public static double getCube(double a){
+       return a*a*a;
+    }
+}

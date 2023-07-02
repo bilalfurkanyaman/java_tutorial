8bc904ca705e0c8676ae1e6d961a6b512eb44379
bilalfurkanyaman <138367431+bilalfurkanyaman@users.noreply.github.com>
2023-07-02 17:33:00 +0300

    11

---
+++ 11-objectolusturma02.java
@@ -0,0 +1,15 @@
+public class Untitled2{
+    public static void main(String[] args) {
+        
+    Main tomHanks = new Main();
+    System.out.println(tomHanks.name);
+    System.out.println(tomHanks.grade);
+    System.out.println(tomHanks.address);
+    
+    tomHanks.feed();
+    tomHanks.study();
+    
+    }
+    
+    
+}

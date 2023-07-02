8ddb41283cae7d2c9609b02385c4a8a06816e1b4
bilalfurkanyaman <138367431+bilalfurkanyaman@users.noreply.github.com>
2023-07-02 17:32:46 +0300

    10

---
+++ 10-objectolusturma01.java
@@ -0,0 +1,18 @@
+
+
+public class Main{
+    
+    
+    public String model = "Tom Hanks";
+    public byte grade = 8;
+    public String address = "Miami";
+
+    public void study(){
+        System.out.println("Gunu gunune ders calisir"); 
+    }
+    public void feed(){
+        System.out.println("Saglikli bir sekilde beslenir");
+        
+    }
+
+}

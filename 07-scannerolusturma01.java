160384d1d50929d4375d6c1b93a81f3730412a74
bilalfurkanyaman <138367431+bilalfurkanyaman@users.noreply.github.com>
2023-07-02 16:43:50 +0300

    07

---
+++ 07-scannerolusturma01.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main (String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Lutfen yasinizi giriniz");
+        
+        byte age = input.nextByte();
+        
+        System.out.println(age);
+    }
+}
+
+
+

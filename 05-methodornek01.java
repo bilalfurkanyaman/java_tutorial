c331230c7ed687c878cf3bdc2722357b252244d1
bilalfurkanyaman <138367431+bilalfurkanyaman@users.noreply.github.com>
2023-07-02 15:00:09 +0300

    05

---
+++ 05-methodornek01.java
@@ -0,0 +1,14 @@
+
+
+public class Main{
+    public static void main(String[] args) {
+       
+       int ucluSonuc = firstTwoMultiplyThirdAdd(2, 3, 5);
+       System.out.println(ucluSonuc);
+    
+    }
+    
+    private static int firstTwoMultiplyThirdAdd(int a, int b, int c){
+        return a*b+c;    
+    }
+}
